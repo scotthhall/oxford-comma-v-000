37535a72b93e4dabfa3218d0c440be78c1c8f255
Scott Hall <scotthall127@gmail.com>
2018-07-16 00:24:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  elsif array.count == 3
+    array[0] << ", "
+    array[1] << ", and "
+    array.join
+  elsif array.count == 5
+    array[0] << ", "
+    array[1] << ", "
+    array[2] << ", "
+    array[3] << ", and "
+    array.join
+  elsif array.count == 7
+    array[0] << ", "
+    array[1] << ", "
+    array[2] << ", "
+    array[3] << ", "
+    array[4] << ", "
+    array[5] << ", and "
+    array.join
+  end
 end
