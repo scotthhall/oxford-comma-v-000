8777076e97a9e0961438ba9b72d66e26ec812271
Scott Hall <scotthall127@gmail.com>
2018-07-16 00:35:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,9 @@
 def oxford_comma(array)
-  last_item = array.pop
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    last_item = array.pop
   array.join(", ") << ", and #{last_item}"
 end
