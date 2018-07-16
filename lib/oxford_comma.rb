f92e9e19fa1c60363205e26b54a2896ce51394e2
Scott Hall <scotthall127@gmail.com>
2018-07-16 00:34:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,25 +1,4 @@
 def oxford_comma(array)
-  if array.count == 1
-    array.join
-  elsif array.count == 2
-    array.join(" and ")
-  elsif array.count == 3
-    array[0] << ", "
-    array[1] << ", and "
-    array.join
-  elsif array.count == 5
-    array[0] << ", "
-    array[1] << ", "
-    array[2] << ", "
-    array[3] << ", and "
-    array.join
-  elsif array.count == 7
-    array[0] << ", "
-    array[1] << ", "
-    array[2] << ", "
-    array[3] << ", "
-    array[4] << ", "
-    array[5] << ", and "
-    array.join
-  end
+  last_item = array.pop
+  array.join(", ") << ", and #{last_item}"
 end
